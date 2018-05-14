4d08bd6a0946200e4c0c6e51889845e6cd8b0bc5
maxiaotao <xiaotao.ma.zbj@gmail.com>
2018-05-14 14:01:46 +0800

    死锁代码示例

--- src/main/java/com/mxt/concurrent/lock/Lock.java
+++ src/main/java/com/mxt/concurrent/lock/Lock.java
@@ -2,7 +2,7 @@ package com.mxt.concurrent.lock;
 
 /**
  * java 锁学习
- *
+ *  https://www.jianshu.com/p/fb6e91b013cc  深入浅出CAS
  *  锁的类型
  *      锁从宏观上分类，分为悲观锁与乐观锁。
  *      乐观锁 是一种乐观思想，即认为读多写少，乐观锁基本都是通过CAS操作实现的
